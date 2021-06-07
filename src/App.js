65ed9d955bc9935f81e7b15f4a56d3b18f969848
AmyLynn87 <e.crescentini87@gmail.com>
2021-06-07 20:38:05 +0200

    Added the github repo url

--- src/App.js
+++ src/App.js
@@ -4,7 +4,11 @@ import './App.css';
 export default function App() {
   return (
     <div className="App">
-      Weather App
+      <h1>Weather App</h1>
+      <footer>
+        This project is created by <a href="https://www.elisacrescentini.dev" target="_blank" rel="noreferrer noopener" title="Elisa Crescentini portfolio">Elisa Crescentini</a> and is {" "} 
+        <a href="https://github.com/AmyLynn87/react-weather-app" target="_blank" rel="noreferrer noopener" title="Weather app with React repository on GitHub">open-sourced</a> on GitHub.
+      </footer>
     </div>
   );
 }
