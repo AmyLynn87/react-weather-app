86393f5f6515c19639dd54536162332d81931080
AmyLynn87 <e.crescentini87@gmail.com>
2021-06-17 11:20:52 +0200

    setting the API call for the forecast

--- src/Weather.js
+++ src/Weather.js
@@ -19,6 +19,7 @@ export default function Weather(props) {
       icon: response.data.weather[0].icon,
       wind: response.data.wind.speed,
       city: response.data.name,
+      coordinates: response.data.coord,
     });
   }
 
@@ -58,7 +59,7 @@ export default function Weather(props) {
   </div>
         </form>
         <WeatherInfo data={weatherData} />
-        <WeatherForecast />
+        <WeatherForecast coordinates={weatherData.coordinates} />
       </div>
     );
   } else {
--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -1,8 +1,24 @@
 import React from "react";
 import WeatherIcon from "./WeatherIcon";
 import "./WeatherForecast.css";
+import axios from "axios";
 
-export default function WeatherForecast() {
+export default function WeatherForecast(props) {
+
+function handleResponse(response) {
+    console.log(response.data);
+}
+
+console.log(props);
+
+    let latitude = props.coordinates.lat;
+    let longitude = props.coordinates.lon;
+    let unit = "metric";
+    let apiKey ="5f472b7acba333cd8a035ea85a0d4d4c";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=${unit}`;
+
+    axios.get(apiUrl).then(handleResponse);
+
     return (
         <div className="WeatherForecast">
             <div className="row">
